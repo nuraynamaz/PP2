b5873cbe5d94af59c26c19c176ad5797fd069f8a
Nuray Namazova <nnamazova20412@ada.edu.az>
2026-02-05 09:55:46 +0400

    student

--- src/ada/student/Student.java
+++ src/ada/student/Student.java
@@ -5,5 +5,10 @@ public class Student {
     public static void main(String[] args)
     {
         System.out.println("Hello Student");
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.nextLine();
+
+        System.out.println(a);
+
     }
 }
